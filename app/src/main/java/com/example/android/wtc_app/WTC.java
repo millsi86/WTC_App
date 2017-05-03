70306d926f85d561b8682bba4c1347d294585435
millsi86 <tommills86@gmail.com>
2017-05-03 11:59:23 +0100

    Tidy up of app for submission

--- app/src/main/java/com/example/android/wtc_app/WTC.java
+++ app/src/main/java/com/example/android/wtc_app/WTC.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import static com.example.android.wtc_app.R.id.address;
-
 public class WTC extends AppCompatActivity {
 
     @Override
@@ -15,9 +13,6 @@ public class WTC extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wtc);
     }
-
-
-
 
     public void Map(View view) {
         Uri gmmMapIntent = Uri.parse("geo:51.5163042,-3.2207376?q=Whitchurch Tennis Club");
@@ -27,24 +22,4 @@ public class WTC extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
-
-
-
-    public void email(View view){
-        String emailAddress =  "info@whitchurchtenniscardiff.co.uk";
-        String title = "Enquiry";
-        composeEmail(emailAddress, title);
-    }
-
-    private void composeEmail(String emailAddress, String subject) {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("*/*)");
-        intent.putExtra(Intent.EXTRA_EMAIL, address);
-        //intent.putExtra(intent.EXTRA_SUBJECT, subject);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
-
-
 }
